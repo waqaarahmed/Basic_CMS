f76545ae21015a33cf7d604cc2bd7db5e00f62e9
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-08-16 23:48:43 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+class Content(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'<Content {self.title}>'
